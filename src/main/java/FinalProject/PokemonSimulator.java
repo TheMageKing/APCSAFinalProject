10e7683ef498734caf54d9797a996aa58f07ee1b
Calum McConnell <calumlikesapplepie@gmail.com>
2020-06-07 23:22:57 -0400

    Setup sim: rip out bits of their class that suck

---
+++ src/main/java/FinalProject/PokemonSimulator.java
@@ -0,0 +1,14 @@
+package FinalProject;
+
+public class PokemonSimulator {
+    PokemonTrainer playerOne;
+    PokemonTrainer playerTwo;
+
+    public void initGame(){
+        
+    }
+
+    private PokemonTrainer setupPokemonTrainer(){
+
+    }
+}
